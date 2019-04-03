f17ff34e549384751958109eaebb5278c191bb9c
Mohammed Elmasry <mohammed.hussein.mahmoud@gmail.com>
2019-04-03 05:36:41 +0200

    create wishlist counter and cart counter components

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './top/header/header.component';
 import { NavigationLinkComponent } from './top/navigation-link/navigation-link.component';
 import { FooterComponent } from './footer/footer.component';
+import { WishCounterComponent } from './top/wish-counter/wish-counter.component';
+import { CartCounterComponent } from './top/cart-counter/cart-counter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavigationLinkComponent,
-    FooterComponent
+    FooterComponent,
+    WishCounterComponent,
+    CartCounterComponent
   ],
   imports: [
     BrowserModule,
---
+++ app/top/wish-counter/wish-counter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wish-counter',
+  templateUrl: './wish-counter.component.html',
+  styleUrls: ['./wish-counter.component.scss']
+})
+export class WishCounterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
