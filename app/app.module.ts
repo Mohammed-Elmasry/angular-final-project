3ab7346cbebd98abe79f24374d4a8970f40b588a
Mohammed Elmasry <mohammed.hussein.mahmoud@gmail.com>
2019-04-03 04:31:50 +0200

    add header component

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './top/header/header.component';
 import { NavigationLinkComponent } from './top/navigation-link/navigation-link.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavigationLinkComponent
+    NavigationLinkComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
