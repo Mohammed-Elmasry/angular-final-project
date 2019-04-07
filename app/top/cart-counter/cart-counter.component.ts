178638ae949f61bdd778b0aff6b078ee0b926fc0
Mohammed Elmasry <mohammed.hussein.mahmoud@gmail.com>
2019-04-07 20:59:34 +0200

    subscribe to Observable returned from getProduct() method of ProductTransferService within constructor of cartCounterComponent's ts file

--- app/top/cart-counter/cart-counter.component.ts
+++ app/top/cart-counter/cart-counter.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Subscription } from 'rxjs';
 import { ProductTransferService } from 'src/app/services/product-transfer.service';
 
 @Component({
@@ -8,12 +8,26 @@ import { ProductTransferService } from 'src/app/services/product-transfer.servic
   styleUrls: ['./cart-counter.component.scss']
 })
 export class CartCounterComponent implements OnInit {
+  public subscription = Subscription;
+  public products = [];
+  public num: number = 0;
 
-  constructor(private transferProduct: ProductTransferService) { }
+  constructor(private transferProduct: ProductTransferService) {
+    this.transferProduct.getProduct().subscribe((product) => {
+      if (product) {
+        this.products.push(product);
+        // console.log("received product: ", product);
+        console.log(this.products);
+        this.num = this.products.length;
+      } else {
+        console.log("no products received");
+      }
+    });
+  }
 
   ngOnInit() {
   }
 
-  
+
 
 }
