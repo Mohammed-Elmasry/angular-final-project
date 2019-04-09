74a445668f5902594f90fb4dc4a8426d0e9449bd
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-04-09 12:13:19 +0200

    add internal methods to Product2wishlist service

--- app/services/product2-wishlist.service.ts
+++ app/services/product2-wishlist.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Product2WishlistService {
+  private subject = new Subject<any>();
+
+  sendProduct(product: Product): void {
+    this.subject.next( product );
+  }
+
+  clearProduct(): void {
+    this.subject.next();
+  }
+
+  getProduct(): Observable<any> {
+    return this.subject.asObservable();
+  }
 
   constructor() { }
 }
