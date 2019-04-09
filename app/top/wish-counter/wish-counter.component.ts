fcfc7450cad7e4a32dc4c7bd8de99b6fd78d7c30
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-04-09 12:23:47 +0200

    add products to wishlist logic within wishCounter Component's ts file

--- app/top/wish-counter/wish-counter.component.ts
+++ app/top/wish-counter/wish-counter.component.ts
@@ -1,13 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Product2WishlistService } from 'src/app/services/product2-wishlist.service';
+import { Product } from 'src/app/models/product';
 @Component({
   selector: 'app-wish-counter',
   templateUrl: './wish-counter.component.html',
   styleUrls: ['./wish-counter.component.scss']
 })
 export class WishCounterComponent implements OnInit {
+  private productList: Product[] = [];
+  private numProducts = 0;
 
-  constructor() { }
+  constructor(private product2wishlist: Product2WishlistService) {
+    this.product2wishlist.getProduct().subscribe((product) => {
+      if (product) {
+        if (this.productList.indexOf(product) === -1) { //product not in wishlist
+          this.productList.push(product);
+          this.numProducts = this.productList.length;
+        } else { //product already added
+          alert("product is already within the wishlist");
+        }
+      } else {
+        console.log("no products received");
+      }
+    });
+  }
 
   ngOnInit() {
   }
