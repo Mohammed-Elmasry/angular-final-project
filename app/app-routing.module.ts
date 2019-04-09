332b5e2f33ed4ddadbaadd1b37c54d37a85c9468
Mohammed Elmasry <mohammed.hussein.mahmoud@gmail.com>
2019-04-09 12:58:45 +0200

    add routing from singleProduct component's html to singleProductDetailsPage component's html file

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './body/home-page/home-page.component';
 import { CartPageComponent } from './body/cart-page/cart-page.component';
 import { WishPageComponent } from './body/wish-page/wish-page.component';
+import { SingleProductDisplayPageComponent } from './body/single-product-display-page/single-product-display-page.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'home', pathMatch:'full'},
   {path:'home', component:HomePageComponent},
   {path:'cart', component:CartPageComponent},
-  {path:'wish', component:WishPageComponent}
+  {path:'wish', component:WishPageComponent},
+  {path:'details', component: SingleProductDisplayPageComponent},
 ];
 
 @NgModule({
