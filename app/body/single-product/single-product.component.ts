f7f4c3ed7d909cb91f2a5a341e5d69d2775407e3
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-04-09 12:14:13 +0200

    add Product2Wishlist service inside singleProductComponent's ts file and define send2wishlist() method

--- app/body/single-product/single-product.component.ts
+++ app/body/single-product/single-product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { ProductTransferService } from 'src/app/services/product-transfer.service';
+import { Product2WishlistService } from 'src/app/services/product2-wishlist.service';
 
 @Component({
   selector: 'app-single-product',
@@ -9,12 +10,16 @@ import { ProductTransferService } from 'src/app/services/product-transfer.servic
 })
 export class SingleProductComponent implements OnInit {
   @Input() product: Product;
-  constructor(private productTransfer: ProductTransferService) { }
+  constructor(private productTransfer: ProductTransferService, private productwish: Product2WishlistService) { }
 
   ngOnInit() {
   }
 
   transferProduct(): void {
     this.productTransfer.sendProduct(this.product);
+  }
+
+  send2Wishlist(): void {
+    this.productwish.sendProduct(this.product);
   }
 }
