034f71f5c836717984b898e49f12bcfebdb6b7cd
Mohammed Elmasry <mohammed.hussein.mahmoud@gmail.com>
2019-04-11 05:31:58 +0200

    add authentication service and guard to the application

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -6,13 +6,14 @@ import { WishPageComponent } from './body/wish-page/wish-page.component';
 import { SingleProductDisplayPageComponent } from './body/single-product-display-page/single-product-display-page.component';
 import { LoginFormComponent } from './forms/login-form/login-form.component';
 import { RegisterComponent } from './forms/register/register.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   {path:'', redirectTo:'home', pathMatch:'full'},
-  {path:'home', component:HomePageComponent},
-  {path:'cart', component:CartPageComponent},
-  {path:'wish', component:WishPageComponent},
-  {path:'details/:id', component: SingleProductDisplayPageComponent},
+  {path:'home', component:HomePageComponent, canActivate:[AuthGuard]},
+  {path:'cart', component:CartPageComponent, canActivate:[AuthGuard]},
+  {path:'wish', component:WishPageComponent, canActivate:[AuthGuard]},
+  {path:'details/:id', component: SingleProductDisplayPageComponent, canActivate:[AuthGuard]},
   {path: 'login', component: LoginFormComponent},
   {path: 'register', component: RegisterComponent}
 ];
